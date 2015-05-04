a93b5117f705ba9fd83cb2ba6665d4c29cc5308a
Jon Turner <jon@jpt.me.uk>
2015-05-04 22:28:08 +0100

    Updated setup.py

--- pychro/setup.py
+++ pychro/setup.py
@@ -4,5 +4,12 @@ setup(name='pychro',
       version='0.2-dev',
       packages=['pychro'],
       package_data={'pychro':['libpychroc.so', 'PychroCLib.dll']},
-      author='Jon Turner')
-      
+      author='Jon Turner',
+      description='Library to access Chronicle-Queue message journals',
+      url='https://github.com/jontuk/pychro',
+      license='Apache 2.0',
+      classifiers=[ 'Development Status :: 3 - Alpha',
+                    'Intended Audience :: Developers',
+                    'License :: OSI Approved :: Apache 2.0',
+                    'Programming Language :: Python :: 3.4' ]
+)
