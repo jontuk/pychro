1213043ebdc67c054d9c9e3107266250600b4aa1
Jon Turner <jon@jpt.me.uk>
2015-05-04 22:37:19 +0100

    Released v0.4

--- pychro/setup.py
+++ pychro/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pychro',
-      version='0.2-dev',
+      version='0.4',
       packages=['pychro'],
       package_data={'pychro':['libpychroc.so', 'PychroCLib.dll']},
       author='Jon Turner',
