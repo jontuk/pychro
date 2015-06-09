d956c0647623b34eab82c5cd4787f5ea62590944
Jon Turner <jon@jpt.me.uk>
2015-06-09 15:59:42 +0100

    Bumped version

--- pychro/setup.py
+++ pychro/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pychro',
-      version='0.6.3',
+      version='0.6.4',
       packages=['pychro'],
       package_data={'pychro':['libpychroc.so', 'PychroCLib.dll']},
       author='Jon Turner',
