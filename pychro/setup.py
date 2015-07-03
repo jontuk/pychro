10a2ea5effc7f663991f787e3ac1b4f1d3e86ea0
Jon Turner <jon@jpt.me.uk>
2015-07-03 14:21:00 +0100

    Don't create a new data file for each new VanillaChronicleWriter on the same thread.

--- pychro/setup.py
+++ pychro/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pychro',
-      version='0.6.6',
+      version='0.6.7',
       packages=['pychro'],
       package_data={'pychro':['libpychroc.so', 'PychroCLib.dll']},
       author='Jon Turner',
