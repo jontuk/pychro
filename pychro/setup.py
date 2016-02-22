9979388266d38900a55151fbe5e9226ecbef417b
Jon Turner <jon@jpt.me.uk>
2016-02-22 21:41:04 +0000

    Bumped version

--- pychro/setup.py
+++ pychro/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup, Extension
 
 setup(name='pychro',
-      version='0.8.0',
+      version='0.8.2',
       packages=['pychro'],
       author='Jon Turner',
       description='Memory-mapped message journal',
