1ab1b9b125d832a8c00205f9a540f28ff7dbbfc1
Jon Turner <jon@jpt.me.uk>
2016-03-25 12:46:38 +0000

    Added xmlrunner to produce xml test reports

--- pychro/test/test_pychro.py
+++ pychro/test/test_pychro.py
@@ -1083,4 +1083,11 @@ class TestGetIndexChronicle(unittest.TestCase):
         pass
 
 if __name__ == '__main__':
-    unittest.main()
+    try:
+        import xmlrunner
+        unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'),
+                      # these make sure that some options that are not applicable
+                      # remain hidden from the help menu.
+                      failfast=False, buffer=False, catchbreak=False)
+    except ImportError:
+        unittest.main()
